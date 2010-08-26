5887846aa121dd2c53ae42667e823e1f1c5c2ed6
Duco Dokter <dokter@w20e.com>
2010-08-26 12:26:41 +0000

    Cleanup layout and panel links from final rendered page.
    svn path=/plone.app.blocks/trunk/; revision=39001

--- plone/app/blocks/panel.py
+++ plone/app/blocks/panel.py
@@ -5,7 +5,7 @@ from urlparse import urljoin
 headerReplace = [etree.XPath("/html/head/%s" % tag) for tag in ('title', 'base',)]
 headerAppend = [etree.XPath("/html/head/%s" % tag) for tag in ('style', 'link', 'script', 'meta')]
 
-def merge(request, pageTree):
+def merge(request, pageTree, removePanelLinks=True, removeLayoutLink=True):
     """Perform panel merging for the given page.
     
     Returns None if the page has no layout.
@@ -43,8 +43,14 @@ def merge(request, pageTree):
             
             if layoutPanelNode is not None and pagePanelNode is not None:
                 layoutPanelNode.getparent().replace(layoutPanelNode, pagePanelNode)
-    
+
+        if removePanelLinks:
+            panelLinkNode.getparent().remove(panelLinkNode)
+                
     # Merge the head of both documents
     utils.mergeHead(pageTree, layoutTree, headerReplace, headerAppend)
+
+    if removeLayoutLink:
+        layoutNode.getparent().remove(layoutNode)
     
     return layoutTree
