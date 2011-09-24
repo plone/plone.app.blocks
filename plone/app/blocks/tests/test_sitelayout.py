f7adad9f77cd4bb642407410cb10147514dfb890
Timo Stollenwerk <contact@timostollenwerk.net>
2011-09-24 15:13:45 +0200

    Refactor/fix sitelayout test.

--- plone/app/blocks/tests/test_sitelayout.py
+++ plone/app/blocks/tests/test_sitelayout.py
@@ -1,80 +1,73 @@
+import transaction
 import unittest2 as unittest
+
+from StringIO import StringIO
+from OFS.Image import File
+
+from zExceptions import NotFound
+
+from zope.component import adapts
+from zope.component import getUtility
+from zope.component import getMultiAdapter
+from zope.component import getSiteManager
+from zope.interface import implements
+
+from Products.CMFPlone.utils import getToolByName
+from Products.BTreeFolder2.BTreeFolder2 import BTreeFolder2
+
+from plone.registry.interfaces import IRegistry
+from plone.memoize.volatile import ATTR
+
+from plone.app.blocks.interfaces import DEFAULT_SITE_LAYOUT_REGISTRY_KEY
+from plone.app.blocks.layoutbehavior import ILayoutAware
+
+from plone.app.testing import setRoles, TEST_USER_ID
+from plone.app.blocks.interfaces import DEFAULT_SITE_LAYOUT_REGISTRY_KEY
 
 from plone.app.blocks.testing import BLOCKS_FUNCTIONAL_TESTING
 
 class TestSiteLayout(unittest.TestCase):
 
     layer = BLOCKS_FUNCTIONAL_TESTING
-    
+
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.request = self.layer['request']
+        self.registry = getUtility(IRegistry)
+        setRoles(self.portal, TEST_USER_ID, ('Manager',))
+        self.portal.invokeFactory('Folder', 'f1', title=u"Folder 1")
+        self.portal['f1'].invokeFactory('Document', 'd1', title=u"Document 1")
+        setRoles(self.portal, TEST_USER_ID, ('Member',))
+            
     def test_default_site_layout_no_registry_key(self):
-        from zope.component import getUtility
-        from zope.component import getMultiAdapter
-        from plone.registry.interfaces import IRegistry
-        from plone.memoize.volatile import ATTR
-        from zExceptions import NotFound
-        from plone.app.blocks.interfaces import DEFAULT_SITE_LAYOUT_REGISTRY_KEY
-        
-        portal = self.layer['portal']
-        request = self.layer['request']
-        
         # Clear cache if there
-        if hasattr(portal, ATTR):
-            delattr(portal, ATTR)
+        if hasattr(self.portal, ATTR):
+            delattr(self.portal, ATTR)
         
-        registry = getUtility(IRegistry)
-        registry[DEFAULT_SITE_LAYOUT_REGISTRY_KEY] = None
+        self.registry[DEFAULT_SITE_LAYOUT_REGISTRY_KEY] = None
         
-        view = getMultiAdapter((portal, request,), name=u'default-site-layout')
+        view = getMultiAdapter((self.portal, self.request,), name=u'default-site-layout')
         self.assertRaises(NotFound, view)
     
     def test_default_site_layout(self):
-        from zope.component import getUtility
-        from zope.component import getMultiAdapter
-        from plone.registry.interfaces import IRegistry
-        from plone.memoize.volatile import ATTR
-        from plone.app.blocks.interfaces import DEFAULT_SITE_LAYOUT_REGISTRY_KEY
-        
-        portal = self.layer['portal']
-        request = self.layer['request']
-        
         # Clear cache if there
-        if hasattr(portal, ATTR):
-            delattr(portal, ATTR)
+        if hasattr(self.portal, ATTR):
+            delattr(self.portal, ATTR)
         
-        registry = getUtility(IRegistry)
-        registry[DEFAULT_SITE_LAYOUT_REGISTRY_KEY] = '/++sitelayout++testlayout1/site.html'
+        self.registry[DEFAULT_SITE_LAYOUT_REGISTRY_KEY] = '/++sitelayout++testlayout1/site.html'
         
-        view = getMultiAdapter((portal, request,), name=u'default-site-layout')
+        view = getMultiAdapter((self.portal, self.request,), name=u'default-site-layout')
         rendered = view()
         
         self.assertTrue(u"Layout title" in rendered)
     
     def test_default_site_layout_section_override(self):
-        from zope.interface import implements
-        from zope.component import getUtility
-        from zope.component import getMultiAdapter
-        from zope.component import getSiteManager
-        from zope.component import adapts
-        from plone.registry.interfaces import IRegistry
-        from plone.app.testing import setRoles
-        from plone.app.testing import TEST_USER_ID
-        from plone.app.blocks.layoutbehavior import ILayoutAware
-        from plone.app.blocks.interfaces import DEFAULT_SITE_LAYOUT_REGISTRY_KEY
-        
-        portal = self.layer['portal']
-        request = self.layer['request']
-        
-        setRoles(portal, TEST_USER_ID, ('Manager',))
-        portal.invokeFactory('Folder', 'f1', title=u"Folder 1")
-        portal['f1'].invokeFactory('Document', 'd1', title=u"Document 1")
-        setRoles(portal, TEST_USER_ID, ('Member',))
-        
-        registry = getUtility(IRegistry)
-        registry[DEFAULT_SITE_LAYOUT_REGISTRY_KEY] = '/++sitelayout++testlayout1/site.html'
+        self.registry[DEFAULT_SITE_LAYOUT_REGISTRY_KEY] = '/++sitelayout++testlayout1/site.html'
         
         class FolderLayoutAware(object):
             implements(ILayoutAware)
-            adapts(portal['f1'].__class__)
+            adapts(self.portal['f1'].__class__)
             
             def __init__(self, context):
                 self.context = context
@@ -87,56 +80,27 @@ class TestSiteLayout(unittest.TestCase):
         sm = getSiteManager()
         sm.registerAdapter(FolderLayoutAware)
         
-        view = getMultiAdapter((portal['f1']['d1'], request,), name=u'default-site-layout')
+        view = getMultiAdapter((self.portal['f1']['d1'], self.request,), name=u'default-site-layout')
         rendered = view()
         
         self.assertFalse(u"Layout title" in rendered)
         self.assertTrue(u"Layout 2 title" in rendered)
     
     def test_default_site_layout_section_no_override(self):
-        from zope.component import getUtility
-        from zope.component import getMultiAdapter
-        from plone.registry.interfaces import IRegistry
-        from plone.app.testing import setRoles
-        from plone.app.testing import TEST_USER_ID
-        from plone.app.blocks.interfaces import DEFAULT_SITE_LAYOUT_REGISTRY_KEY
+        self.registry[DEFAULT_SITE_LAYOUT_REGISTRY_KEY] = '/++sitelayout++testlayout1/site.html'
         
-        portal = self.layer['portal']
-        request = self.layer['request']
-        
-        setRoles(portal, TEST_USER_ID, ('Manager',))
-        portal.invokeFactory('Folder', 'f1', title=u"Folder 1")
-        portal['f1'].invokeFactory('Document', 'd1', title=u"Document 1")
-        setRoles(portal, TEST_USER_ID, ('Member',))
-        
-        registry = getUtility(IRegistry)
-        registry[DEFAULT_SITE_LAYOUT_REGISTRY_KEY] = '/++sitelayout++testlayout1/site.html'
-        
-        view = getMultiAdapter((portal['f1']['d1'], request,), name=u'default-site-layout')
+        view = getMultiAdapter((self.portal['f1']['d1'], self.request,), name=u'default-site-layout')
         rendered = view()
         
         self.assertFalse(u"Layout 2 title" in rendered)
         self.assertTrue(u"Layout title" in rendered)
     
     def test_default_site_layout_cache(self):
-        from zope.component import getUtility
-        from zope.component import getMultiAdapter
-        from plone.registry.interfaces import IRegistry
-        from plone.memoize.volatile import ATTR
-        from Products.CMFPlone.utils import getToolByName
-        from Products.BTreeFolder2.BTreeFolder2 import BTreeFolder2
-        from StringIO import StringIO
-        from OFS.Image import File
-        from plone.app.blocks.interfaces import DEFAULT_SITE_LAYOUT_REGISTRY_KEY
-        
-        portal = self.layer['portal']
-        request = self.layer['request']
-        
         # Clear cache if there
-        if hasattr(portal, ATTR):
-            delattr(portal, ATTR)
+        if hasattr(self.portal, ATTR):
+            delattr(self.portal, ATTR)
         
-        resources = getToolByName(portal, 'portal_resources')
+        resources = getToolByName(self.portal, 'portal_resources')
         resources._setOb('sitelayout', BTreeFolder2('sitelayout'))
         resources['sitelayout']._setOb('testlayout3', BTreeFolder2('testlayout3'))
         resources['sitelayout']['testlayout3']._setOb('site.html',
@@ -145,10 +109,9 @@ class TestSiteLayout(unittest.TestCase):
                         )
             )
         
-        registry = getUtility(IRegistry)
-        registry[DEFAULT_SITE_LAYOUT_REGISTRY_KEY] = '/++sitelayout++testlayout3/site.html'
+        self.registry[DEFAULT_SITE_LAYOUT_REGISTRY_KEY] = '/++sitelayout++testlayout3/site.html'
         
-        view = getMultiAdapter((portal, request,), name=u'default-site-layout')
+        view = getMultiAdapter((self.portal, self.request,), name=u'default-site-layout')
         rendered = view()
         
         self.assertTrue(u"ZODB test" in rendered)
@@ -160,32 +123,18 @@ class TestSiteLayout(unittest.TestCase):
                         )
             )
         
-        view = getMultiAdapter((portal, request,), name=u'default-site-layout')
+        view = getMultiAdapter((self.portal, self.request,), name=u'default-site-layout')
         rendered = view()
         
         self.assertFalse(u"Cache test" in rendered) # hidden by cache
         self.assertTrue(u"ZODB test" in rendered)
     
     def test_default_site_layout_invalidate_mtime(self):
-        import transaction
-        from zope.component import getUtility
-        from zope.component import getMultiAdapter
-        from plone.registry.interfaces import IRegistry
-        from plone.memoize.volatile import ATTR
-        from Products.CMFPlone.utils import getToolByName
-        from Products.BTreeFolder2.BTreeFolder2 import BTreeFolder2
-        from StringIO import StringIO
-        from OFS.Image import File
-        from plone.app.blocks.interfaces import DEFAULT_SITE_LAYOUT_REGISTRY_KEY
-        
-        portal = self.layer['portal']
-        request = self.layer['request']
-        
         # Clear cache if there
-        if hasattr(portal, ATTR):
-            delattr(portal, ATTR)
+        if hasattr(self.portal, ATTR):
+            delattr(self.portal, ATTR)
         
-        resources = getToolByName(portal, 'portal_resources')
+        resources = getToolByName(self.portal, 'portal_resources')
         resources._setOb('sitelayout', BTreeFolder2('sitelayout'))
         resources['sitelayout']._setOb('testlayout3', BTreeFolder2('testlayout3'))
         resources['sitelayout']['testlayout3']._setOb('site.html',
@@ -194,16 +143,15 @@ class TestSiteLayout(unittest.TestCase):
                         )
             )
         
-        registry = getUtility(IRegistry)
-        registry[DEFAULT_SITE_LAYOUT_REGISTRY_KEY] = '/++sitelayout++testlayout3/site.html'
+        self.registry[DEFAULT_SITE_LAYOUT_REGISTRY_KEY] = '/++sitelayout++testlayout3/site.html'
         
-        view = getMultiAdapter((portal, request,), name=u'default-site-layout')
+        view = getMultiAdapter((self.portal, self.request,), name=u'default-site-layout')
         rendered = view()
         
         self.assertTrue(u"ZODB test" in rendered)
         
         # Trigger invalidation by modifying the context and committing
-        portal.title = u"New title"
+        self.portal.title = u"New title"
         transaction.commit()
         
         # Modify the site layout
@@ -214,38 +162,28 @@ class TestSiteLayout(unittest.TestCase):
                         )
             )
         
-        view = getMultiAdapter((portal, request,), name=u'default-site-layout')
+        view = getMultiAdapter((self.portal, self.request,), name=u'default-site-layout')
         rendered = view()
         
         self.assertTrue(u"Cache test" in rendered)
         self.assertFalse(u"ZODB test" in rendered)
     
     def test_default_site_layout_invalidate_registry_key(self):
-        from zope.component import getUtility
-        from zope.component import getMultiAdapter
-        from plone.registry.interfaces import IRegistry
-        from plone.memoize.volatile import ATTR
-        from plone.app.blocks.interfaces import DEFAULT_SITE_LAYOUT_REGISTRY_KEY
-        
-        portal = self.layer['portal']
-        request = self.layer['request']
-        
         # Clear cache if there
-        if hasattr(portal, ATTR):
-            delattr(portal, ATTR)
+        if hasattr(self.portal, ATTR):
+            delattr(self.portal, ATTR)
         
-        registry = getUtility(IRegistry)
-        registry[DEFAULT_SITE_LAYOUT_REGISTRY_KEY] = '/++sitelayout++testlayout1/site.html'
+        self.registry[DEFAULT_SITE_LAYOUT_REGISTRY_KEY] = '/++sitelayout++testlayout1/site.html'
         
-        view = getMultiAdapter((portal, request,), name=u'default-site-layout')
+        view = getMultiAdapter((self.portal, self.request,), name=u'default-site-layout')
         rendered = view()
         
         self.assertTrue(u"Layout title" in rendered)
         
         # Trigger invalidation by modifying the global site layout selection
-        registry[DEFAULT_SITE_LAYOUT_REGISTRY_KEY] = '/++sitelayout++testlayout2/mylayout.html'
+        self.registry[DEFAULT_SITE_LAYOUT_REGISTRY_KEY] = '/++sitelayout++testlayout2/mylayout.html'
         
-        view = getMultiAdapter((portal, request,), name=u'default-site-layout')
+        view = getMultiAdapter((self.portal, self.request,), name=u'default-site-layout')
         rendered = view()
         
         self.assertTrue(u"Layout 2 title" in rendered)
