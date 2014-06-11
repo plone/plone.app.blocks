6ce64e24f8ef47be53b8e97203ece8c35382dab4
Asko Soukka <asko.soukka@iki.fi>
2014-06-11 10:32:58 +0200

    Add default value adapter for ILayoutAware.content

--- plone/app/blocks/layoutbehavior.py
+++ plone/app/blocks/layoutbehavior.py
@@ -1,3 +1,8 @@
+from Products.CMFCore.utils import getToolByName
+from z3c.form.interfaces import IAddForm
+from z3c.form.widget import ComputedWidgetAttribute
+from zExceptions import NotFound
+from zope.component.hooks import getSite
 from zope.interface import implements, alsoProvides, Interface
 
 from zope import schema
@@ -6,6 +11,9 @@ from plone.app.blocks.interfaces import IOmittedField
 from plone.app.blocks.interfaces import ILayoutField
 
 from plone.app.blocks.interfaces import _
+
+import logging
+logger = logging.getLogger('plone.app.blocks')
 
 
 class LayoutField(schema.Text):
@@ -47,3 +55,30 @@ except ImportError:
 alsoProvides(ILayoutAware['content'], IOmittedField)
 alsoProvides(ILayoutAware['pageSiteLayout'], IOmittedField)
 alsoProvides(ILayoutAware['sectionSiteLayout'], IOmittedField)
+
+
+def getDefaultPageLayout(adapter):
+    portal_type = getattr(adapter.view, 'portal_type', None)
+    if not portal_type:
+        return u''
+
+    types_tool = getToolByName(getSite(), 'portal_types')
+    fti = getattr(types_tool, portal_type, None)
+    if fti is None:
+        return u''
+
+    aliases = fti.getMethodAliases() or {}
+    layout = aliases.get('layout')
+
+    if layout:
+        # XXX: p.a.b.utils is importing this module
+        from plone.app.blocks.utils import resolveResource
+        try:
+            return resolveResource(layout)
+        except NotFound as e:
+            logger.warning('Missing layout {0:s}'.format(e))
+    return u''
+
+
+default_layout = ComputedWidgetAttribute(
+    getDefaultPageLayout, view=IAddForm, field=ILayoutField)
