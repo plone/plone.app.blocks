67d2749e9415fc9782b9efc107d0a341e3c461c2
Asko Soukka <asko.soukka@iki.fi>
2014-06-13 13:38:34 +0200

    Fix plone.app.blocks re-install to not reset existing plone.defaultSiteLayout and plone.defaultAjaxLayout settings (by setting the values in a custom setuphandler)

---
+++ plone/app/blocks/setuphandlers.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from plone.registry import Record, field
+from plone.registry.interfaces import IRegistry
+from zope.component import getUtility
+
+
+def step_setup_various(context):
+    if context.readDataFile('plone.app.blocks_default.txt') is None:
+        return
+    portal = context.getSite()
+    initialize_default_layout_registry_values(portal)
+
+
+def initialize_default_layout_registry_values(portal):
+    registry = getUtility(IRegistry)
+    records = (
+        ('plone.defaultSiteLayout', u'Default site layout',
+         u'The default site layout for the site', None),
+        ('plone.defaultAjaxLayout', u'Default ajax layout',
+         u'The default ajax layout for the site', None),
+    )
+    for key, title, description, value in records:
+        if not key in registry.records:
+            registry.records[key] = Record(
+                field.TextLine(
+                    title=title,
+                    description=description
+                ), value)
