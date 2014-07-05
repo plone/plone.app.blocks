9f1927a7bb6baea38312849b48e2f8045f3b67cf
Asko Soukka <asko.soukka@iki.fi>
2014-07-05 06:26:11 +0300

    Refactor tile rules transform into utils

--- plone/app/blocks/utils.py
+++ plone/app/blocks/utils.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
+import logging
+
 from AccessControl import getSecurityManager
 from Acquisition import aq_inner
 from Acquisition import aq_parent
+from diazo import rules, cssrules, compiler
 from lxml import etree
 from lxml import html
-from plone.app.blocks.interfaces import DEFAULT_SITE_LAYOUT_REGISTRY_KEY
-from plone.app.blocks.interfaces import DEFAULT_AJAX_LAYOUT_REGISTRY_KEY
-from plone.app.blocks.interfaces import IOmittedField
-from plone.app.blocks.layoutbehavior import ILayoutAware
+from diazo import utils
 from plone.autoform.interfaces import OMITTED_KEY
 from plone.autoform.interfaces import WIDGETS_KEY
 from plone.autoform.interfaces import MODES_KEY
@@ -15,7 +15,6 @@ from plone.autoform.interfaces import READ_PERMISSIONS_KEY
 from plone.autoform.interfaces import WRITE_PERMISSIONS_KEY
 from plone.autoform.utils import mergedTaggedValuesForIRO
 from plone.registry.interfaces import IRegistry
-from plone.subrequest import subrequest
 from plone.supermodel.utils import mergedTaggedValueDict
 from zExceptions import NotFound
 from z3c.form.interfaces import IEditForm
@@ -29,7 +28,11 @@ from zope.security.interfaces import IPermission
 from zope.site.hooks import getSite
 from zope.schema.interfaces import IField
 
-import logging
+from plone.app.blocks.interfaces import DEFAULT_SITE_LAYOUT_REGISTRY_KEY
+from plone.app.blocks.interfaces import DEFAULT_AJAX_LAYOUT_REGISTRY_KEY
+from plone.app.blocks.interfaces import IOmittedField
+from plone.app.blocks.layoutbehavior import ILayoutAware
+from plone.subrequest import subrequest
 
 
 headXPath = etree.XPath("/html/head")
@@ -293,3 +296,102 @@ def extractFieldInformation(schema, context, request, prefix):
                     'widget': _getWidgetName(schema[name], widgets, request),
                     'readonly': name in read_only,
                 }
+
+
+def add_theme(rules_doc, theme_doc, absolute_prefix=None):
+    if absolute_prefix is None:
+        absolute_prefix = ''
+    root = rules_doc.getroot()
+    element = root.makeelement(
+        rules.fullname(rules.namespaces['diazo'], 'theme'))
+    root.append(element)
+    rules.expand_theme(element, theme_doc, absolute_prefix)
+    return rules_doc
+
+
+def process_rules(rules_doc, theme=None, trace=None, css=True,
+                  absolute_prefix=None, includemode=None,
+                  update=True, stop=None):
+    if trace:
+        trace = '1'
+    else:
+        trace = '0'
+    if stop == 0:
+        return rules_doc
+    if stop == 1:
+        return rules_doc
+    rules_doc = rules.add_identifiers(rules_doc)
+    if stop == 2 or stop == 'add_identifiers':
+        return rules_doc
+    if update:
+        rules_doc = rules.update_namespace(rules_doc)
+    if stop == 3:
+        return rules_doc
+    if css:
+        rules_doc = cssrules.convert_css_selectors(rules_doc)
+    if stop == 4:
+        return rules_doc
+    rules_doc = rules.fixup_theme_comment_selectors(rules_doc)
+    if stop == 5:
+        return rules_doc
+    if theme is not None:
+        rules_doc = add_theme(rules_doc, theme, absolute_prefix)
+    if stop == 6:
+        return rules_doc
+    if includemode is None:
+        includemode = 'document'
+    includemode = "'%s'" % includemode
+    rules_doc = rules.normalize_rules(rules_doc, includemode=includemode)
+    if stop == 7:
+        return rules_doc
+    rules_doc = rules.apply_conditions(rules_doc)
+    if stop == 8:
+        return rules_doc
+    rules_doc = rules.merge_conditions(rules_doc)
+    if stop == 9:
+        return rules_doc
+    rules_doc = rules.fixup_themes(rules_doc)
+    if stop == 10:
+        return rules_doc
+    rules_doc = rules.annotate_themes(rules_doc)
+    if stop == 11:
+        return rules_doc
+    rules_doc = rules.annotate_rules(rules_doc)
+    if stop == 12:
+        return rules_doc
+    rules_doc = rules.apply_rules(rules_doc, trace=trace)
+    return rules_doc
+
+
+def compile_theme(rules_doc, theme_doc=None, css=True,
+                  absolute_prefix=None, update=True, trace=False,
+                  includemode=None, parser=None, compiler_parser=None):
+    rules_doc = process_rules(
+        rules_doc=rules_doc,
+        theme=theme_doc,
+        css=css,
+        absolute_prefix=absolute_prefix,
+        update=update,
+        trace=trace,
+        includemode=includemode,
+    )
+
+    # Build a document with all the <xsl:param /> values to set the defaults
+    # for every value passed in as xsl_params
+    known_params = compiler.build_xsl_params_document({})
+
+    # Create a pseudo resolver for this
+    known_params_url = 'file:///__diazo_known_params__'
+    emit_stylesheet_resolver = utils.CustomResolver({
+        known_params_url: etree.tostring(known_params)})
+    emit_stylesheet_parser = etree.XMLParser()
+    emit_stylesheet_parser.resolvers.add(emit_stylesheet_resolver)
+
+    # Run the final stage compiler
+    emit_stylesheet = utils.pkg_xsl(
+        'emit-stylesheet.xsl', parser=emit_stylesheet_parser)
+    compiled_doc = emit_stylesheet(rules_doc)
+    compiled_doc = compiler.set_parser(
+        etree.tostring(compiled_doc), parser, compiler_parser)
+
+    return compiled_doc
