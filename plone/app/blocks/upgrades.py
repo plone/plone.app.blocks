bc52ebbd02fc1ad3215e9e8ad2e81a0ffacb7e3e
vangheem <vangheem@gmail.com>
2015-09-02 14:57:34 -0500

    add upgrade step

--- plone/app/blocks/upgrades.py
+++ plone/app/blocks/upgrades.py
@@ -8,3 +8,8 @@ PROFILE_ID = 'profile-plone.app.blocks:default'
 def upgrade_settings(context):
     setup = getToolByName(context, 'portal_setup')
     setup.runImportStepFromProfile(PROFILE_ID, 'plone.app.registry')
+
+
+def upgrade_rolemap(context):
+    setup = getToolByName(context, 'portal_setup')
+    setup.runImportStepFromProfile(PROFILE_ID, 'rolemap')
