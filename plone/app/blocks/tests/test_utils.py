3c87656ea09e6d299eb29e6e853d28a7c881d362
Asko Soukka <asko.soukka@iki.fi>
2017-11-03 14:27:41 +0200

    Fix issue where resolving layout url with ajax_load parameter caused fail on direct resolve directory lookup

--- plone/app/blocks/tests/test_utils.py
+++ plone/app/blocks/tests/test_utils.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+from plone.app.blocks.testing import BLOCKS_FUNCTIONAL_TESTING
 from plone.app.blocks.utils import resolve
+from plone.app.blocks.utils import resolveResource
 
 import unittest
 
@@ -32,3 +34,16 @@ class TestUtils(unittest.TestCase):
 </html>""".encode('utf-8')
         text = resolve('', content_layout).xpath('//h1')[0].text
         self.assertEqual(u'Ä', text)
+
+
+class TestUtilsFunctional(unittest.TestCase):
+
+    layer = BLOCKS_FUNCTIONAL_TESTING
+
+    def test_resolve_resource(self):
+        layout = resolveResource('/++sitelayout++testlayout1/site.html')
+        self.assertIn(u'Layout panel 1', layout)
+
+    def test_resolve_resource_with_query(self):
+        layout = resolveResource('/++sitelayout++testlayout1/site.html?ajax_load=1')
+        self.assertIn(u'Layout panel 1', layout)
