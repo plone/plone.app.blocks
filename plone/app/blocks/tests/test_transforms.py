a7c7c126f92108263ca815954339b07e461fadf9
Maurits van Rees <maurits@vanrees.org>
2021-05-06 17:29:19 +0200

    Refactored test_transforms to not have so much duplicate code.
    And prepared this file for having another TestCase class.

--- plone/app/blocks/tests/test_transforms.py
+++ plone/app/blocks/tests/test_transforms.py
@@ -10,56 +10,23 @@ import unittest
 
 
 @implementer(IBlocksTransformEnabled)
-class TestTransformedView(object):
+class TransformedView(object):
     def __init__(self, ret_body):
-        self.__call__ = lambda b=ret_body: b
+        self.body = ret_body
+
+    def __call__(self):
+        return self.body
 
 
-class TestTransforms(unittest.TestCase):
-
+class BaseTestCase(unittest.TestCase):
     layer = BLOCKS_INTEGRATION_TESTING
 
-    def test_transforms_with_crlf(self):
-        """Test fix for issue where layouts with CR[+LF] line-endings are
-        somehow turned into having &#13; line-endings and getting their heads
-        being dropped
-        """
-
-        @implementer(IBlocksTransformEnabled)
-        class TransformedView(object):
-            def __init__(self, ret_body):
-                self.__call__ = lambda b=ret_body: b
-
-        body = """\
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">&#13;
-<head></head>&#13;
-<body></body>&#13;
-</html>"""
-        request = self.layer["request"]
-        request.set("PUBLISHED", TransformedView(body))
-        request.response.setBase(request.getURL())
-        request.response.setHeader("content-type", "text/html")
-        request.response.setBody(body)
-
-        alsoProvides(request, IBlocksLayer)
-        result = applyTransform(request)
-        self.assertIn("<head>", "".join(str(result)))
-
-    def test_transforms_with_cdata(self):
-        """Test fix for issue where layouts with inline js got rendered with
-        quoted (and therefore broken) <![CDATA[...]]> block
-        """
-
-        @implementer(IBlocksTransformEnabled)
-        class TransformedView(object):
-            def __init__(self, ret_body):
-                self.__call__ = lambda b=ret_body: b
-
-        body = """\
+    def prepare_request(self, body=None):
+        if body is None:
+            body = """\
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head><script type="text/javascript"><![CDATA[]]></script></head>
+<head><title>Empty</title></head>
 <body></body>
 </html>"""
         request = self.layer["request"]
@@ -67,7 +34,36 @@ class TestTransforms(unittest.TestCase):
         request.response.setBase(request.getURL())
         request.response.setHeader("content-type", "text/html")
         request.response.setBody(body)
-
         alsoProvides(request, IBlocksLayer)
+        return request
+
+
+class TestTransforms(BaseTestCase):
+    def test_transforms_with_crlf(self):
+        """Test fix for issue where layouts with CR[+LF] line-endings are
+        somehow turned into having &#13; line-endings and getting their heads
+        being dropped
+        """
+        body = """\
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">&#13;
+<head></head>&#13;
+<body></body>&#13;
+</html>"""
+        request = self.prepare_request(body)
+        result = applyTransform(request)
+        self.assertIn("<head>", "".join(str(result)))
+
+    def test_transforms_with_cdata(self):
+        """Test fix for issue where layouts with inline js got rendered with
+        quoted (and therefore broken) <![CDATA[...]]> block
+        """
+        body = """\
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head><script type="text/javascript"><![CDATA[]]></script></head>
+<body></body>
+</html>"""
+        request = self.prepare_request(body)
         result = applyTransform(request)
         self.assertIn("<![CDATA[]]>", "".join(str(result)))
