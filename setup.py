0d4439a6f5a11451479f3e3f97c67d6e60df240a
Maurits van Rees <maurits@vanrees.org>
2022-07-20 13:19:21 +0200

    Back to development: 6.0.2
    [ci skip]

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ long_description = "\n".join(
 
 setup(
     name="plone.app.blocks",
-    version="6.0.1",
+    version="6.0.2.dev0",
     description="Implements the in-Plone blocks rendering process",
     long_description=long_description,
     classifiers=[
